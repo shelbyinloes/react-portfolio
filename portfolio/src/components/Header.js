c3cd2427db44de8bee4de8c7c27eae6d273baff3
Shelby Inloes <shelbyinloes@gmail.com>
2018-09-13 19:34:25 -0500

    new background + nav bar start

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,19 +1,33 @@
 import React, {Component} from "react";
 
-const headStyle = {
-  backgroundColor: "#DE9284",
+const headWrap = {
   marginTop: 0,
   height: "80px",
   width: "100%",
   position: "fixed",
-  borderBottom: "dotted white 5px"
+  display: "flex",
+  justifyContent: "flex-end"
+}
+
+const linkWrap = {
+  width: "40%",
+  height: "100px",
+  display: "flex", 
+  justifyContent: "space-between",
+  marginRight: "50px",
+  fontSize: "13px",
+  alignItems: "center"
 }
 
 class Header extends Component {
   render(){
     return(
-      <div>
-        <p style={headStyle}>hellooo</p>
+      <div style={headWrap}>
+        <div style={linkWrap}>
+          <h1>ABOUT ME</h1>
+          <h1>PORTFOLIO</h1>
+          <h1>CONTACT</h1>
+        </div>
       </div>
     )}
 } 
