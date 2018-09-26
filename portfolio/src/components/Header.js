fa3029786be04c8738e583add0c2788ad29c9eaf
Shelby Inloes <shelbyinloes@gmail.com>
2018-09-26 13:22:14 -0500

    added react bootstrap

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,33 +1,37 @@
 import React, {Component} from "react";
+import {Grid, Row, Col} from "react-bootstrap";
 
-const headWrap = {
-  marginTop: 0,
-  height: "80px",
-  width: "100%",
-  position: "fixed",
-  display: "flex",
-  justifyContent: "flex-end"
-}
+// const headWrap = {
+//   marginTop: 0,
+//   height: "80px",
+//   width: "100%",
+//   position: "fixed",
+//   display: "flex",
+//   justifyContent: "flex-end"
+// }
 
-const linkWrap = {
-  width: "40%",
-  height: "100px",
-  display: "flex", 
-  justifyContent: "space-between",
-  marginRight: "50px",
-  fontSize: "13px",
-  alignItems: "center"
-}
+// const linkWrap = {
+//   width: "40%",
+//   height: "100px",
+//   display: "flex", 
+//   justifyContent: "space-between",
+//   marginRight: "50px",
+//   fontSize: "13px",
+//   alignItems: "center"
+// }
 
 class Header extends Component {
   render(){
     return(
-      <div style={headWrap}>
-        <div style={linkWrap}>
-          <h1>ABOUT ME</h1>
-          <h1>PORTFOLIO</h1>
-          <h1>CONTACT</h1>
-        </div>
+      <div>
+      <Grid>
+        <Row>
+          <Col md={6} sm={0}/>
+          <Col md={2} sm={4}>PORTFOLIO</Col>
+          <Col md={2} sm={4}>ABOUT ME</Col>
+          <Col md={2} sm={4}>CONTACT</Col>
+        </Row>
+      </Grid>
       </div>
     )}
 } 
