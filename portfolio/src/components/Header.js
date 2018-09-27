7839c1e785a64611d5096b77d8997bb9637bc33a
Shelby Inloes <shelbyinloes@gmail.com>
2018-09-26 22:28:44 -0500

    messing w/ bootstrap a little

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,35 +1,21 @@
 import React, {Component} from "react";
 import {Grid, Row, Col} from "react-bootstrap";
 
-// const headWrap = {
-//   marginTop: 0,
-//   height: "80px",
-//   width: "100%",
-//   position: "fixed",
-//   display: "flex",
-//   justifyContent: "flex-end"
-// }
+const headerLinks = {
+  fontSize: "40px"
+}
 
-// const linkWrap = {
-//   width: "40%",
-//   height: "100px",
-//   display: "flex", 
-//   justifyContent: "space-between",
-//   marginRight: "50px",
-//   fontSize: "13px",
-//   alignItems: "center"
-// }
 
 class Header extends Component {
   render(){
     return(
       <div>
       <Grid>
-        <Row>
-          <Col md={6} sm={0}/>
-          <Col md={2} sm={4}>PORTFOLIO</Col>
-          <Col md={2} sm={4}>ABOUT ME</Col>
-          <Col md={2} sm={4}>CONTACT</Col>
+        <Row style={headerLinks}>
+          <Col md={3} sm={0}/>
+          <Col md={3} sm={4}>PORTFOLIO</Col>
+          <Col md={3} sm={4}>ABOUT ME</Col>
+          <Col md={3} sm={4}>CONTACT</Col>
         </Row>
       </Grid>
       </div>
